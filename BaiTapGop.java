5232bd6e677ce84a22e3fd6e0362b278e639d904
Bui Quang Thuan <fingum113@gmail.com>
2025-08-15 21:47:51 +0700

    1

---
+++ BaiTapGop.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class BaiTapGop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // --- Bài 2.4: Tính diện tích hình tròn ---
+        System.out.print("Nhập bán kính r: ");
+        double r = sc.nextDouble();
+        double dienTich = Math.PI * r * r;
+        System.out.println("Diện tích hình tròn là: " + dienTich);
+
+        System.out.println("------------------------------");
+
+        // --- Bài 2.5: Tính tổng từ 1 đến n ---
+        System.out.print("Nhập n: ");
+        int n = sc.nextInt();
+        int tong = 0;
+        for (int i = 1; i <= n; i++) {
+            tong += i;
+        }
+        System.out.println("Tổng từ 1 đến " + n + " là: " + tong);
+
+        sc.close();
+    }
+}
